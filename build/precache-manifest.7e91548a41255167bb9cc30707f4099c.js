0fe731c77b2838e6958c430eeb09c2202121823e
uroos12345 <52444686+uroos12345@users.noreply.github.com>
2020-08-13 13:07:32 +0500

    Add files via upload

---
+++ build/precache-manifest.7e91548a41255167bb9cc30707f4099c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde59d324b9d155953b5dac71983d93e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70411590ba76d6b7a3c6",
+    "url": "/static/css/main.fdd9d27d.chunk.css"
+  },
+  {
+    "revision": "10a48e5275e9c9c93811",
+    "url": "/static/js/2.9805f56a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9805f56a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70411590ba76d6b7a3c6",
+    "url": "/static/js/main.5e59449f.chunk.js"
+  },
+  {
+    "revision": "6379af5bb8b85b3566a0",
+    "url": "/static/js/runtime-main.09c02227.js"
+  }
+]);
